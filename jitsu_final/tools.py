c0db78bb4c490634a45c340048e2d5ef62fbaa88
Maksim Smirnov <smirn08m@gmail.com>
2020-06-24 22:46:51 +0300

    clean code

--- jitsu_final/tools.py
+++ jitsu_final/tools.py
@@ -1,38 +1,11 @@
-import os
-
 import gspread
-import requests
-
-
-class TgApiClient:
-    BASE_URL = 'https://api.telegram.org'
-
-    def __init__(self, token: str, chat_id: str) -> None:
-        self.token = token
-        self.base_url = f'{self.BASE_URL}/bot{token}'
-        self.chat_id = chat_id
-
-    def send_message(
-        self,
-        text: str,
-    ):
-        payload = {
-            'chat_id': self.chat_id,
-            'text': text,
-            'parse_mode': 'Markdown',
-            'disable_notification': True,
-        }
-        response = requests.post(
-            f'{self.base_url}/sendMessage', json=payload, timeout=3
-        )
-        return response.json()
 
 
 class SpreadSheetClient:
-    #DEFAULT_CREDENTIALS_PATH = '~/.config/gspread/service_account.json'
+    # DEFAULT_CREDENTIALS_PATH = '~/.config/gspread/service_account.json'
 
     def __init__(self, creds_path=None):
-        #if creds_path is None:
+        # if creds_path is None:
         #    creds_path = self.DEFAULT_CREDENTIALS_PATH
         if creds_path is not None:
             self.gc = gspread.service_account(filename=creds_path)
@@ -42,4 +15,3 @@ class SpreadSheetClient:
     def open_spreadsheet(self, spreadsheet_name: str):
         self.sh = self.gc.open(spreadsheet_name)
         return self.sh
-
