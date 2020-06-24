65ecd4e0800f4f10aeef9870e4ccf888ec157dd1
Maksim Smirnov <smirn08m@gmail.com>
2020-06-24 22:44:16 +0300

    update readme

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def load_requirements(*requirements_paths):
     return list(requirements)
 
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
